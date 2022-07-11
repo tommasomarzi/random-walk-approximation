3938de3066104396c867cfbfc6427f42a68b69c1
Tommaso Marzi <marzitommaso@gmail.com>
2022-07-11 16:17:45 +0200

    Added normalization

--- error_comparison_JS.py
+++ error_comparison_JS.py
@@ -108,20 +108,30 @@ for (v_2,v_3) in ([1.82, 1.82], [2.47, 2.47], [3.04, 3.04]):
                     data_RKI = np.where(data_RKI<0,0,data_RKI)
                 except:
                     data_RKI = np.zeros(np.shape(data_MUL))
-                    
+
         if N < N_max:           #interpolation
             axis_cur = (np.arange(N+1))/N
-            axis_max = (np.arange(N_max+1))/N_max           
+            axis_max = (np.arange(N_max+1))/N_max  
+
             itp = interp2d(axis_cur, axis_cur, data_MUL)
-            data_MUL = itp(axis_max, axis_max)      
+            data_MUL = itp(axis_max, axis_max) 
+            data_MUL = data_MUL/data_MUL.sum()
+
             itp = interp2d(axis_cur, axis_cur, data_RKI)
-            data_RKI = itp(axis_max, axis_max)      
+            data_RKI = itp(axis_max, axis_max)  
+            data_RKI = data_RKI/data_RKI.sum()  
+
             itp = interp2d(axis_cur, axis_cur, data_LNA)
             data_LNA = itp(axis_max, axis_max)      
+            data_LNA = data_LNA/data_LNA.sum()
+
             itp = interp2d(axis_cur, axis_cur, data_MUL_star)
             data_MUL_star = itp(axis_max, axis_max)      
+            data_MUL_star = data_MUL_star/data_MUL_star.sum()
+
             itp = interp2d(axis_cur, axis_cur, data_GIL)
             data_GIL = itp(axis_max, axis_max)
+            data_GIL = data_GIL/data_GIL.sum()
     
         error_L1_MUL[i] = np.mean(abs(data_MUL-data_RKI))
         error_L2_MUL[i] = np.sqrt(np.mean((data_MUL-data_RKI)**2))
